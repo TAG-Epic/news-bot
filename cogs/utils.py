d30c3d82c5e7f9fdd958c61b59d15e687c556c78
TAG-Epic <tagepicuwu@gmail.com>
2021-02-12 15:23:18 +0100

    Fix: Don't embed links

--- cogs/utils.py
+++ cogs/utils.py
@@ -5,4 +5,4 @@ from speedian.annotations import command
 class Utils(Cog):
     @command(description="Get links to the bot")
     async def invite(self, ctx: CommandContext):
-        await ctx.send("Bot invite: https://vote.rip/news-invite\nSupport server: https://vote.rip/news-support")
+        await ctx.send("Bot invite: <https://vote.rip/news-invite>\nSupport server: <https://vote.rip/news-support>")
